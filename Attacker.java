0437a37518d618f8d12b10f4f37478e334d4851f
Robin Zhang <rzhang19@users.noreply.github.com>
2018-06-26 16:31:55 -0400

    Added comments
    Added comments to variables and methods to make more understandable

--- Attacker.java
+++ Attacker.java
@@ -1,27 +1,45 @@
 public class Attacker extends Player {
-   private static final int SHO_WEIGHT = 5;
-   private static final int ANT_WEIGHT = 5;
-   private static final int BCT_WEIGHT = 3;
-   private static final int PAS_WEIGHT = 3;
-   private static final int TAC_WEIGHT = 2;
-   private static final int INT_WEIGHT = 1;
-   private static final int GKR_WEIGHT = 1;
-   private static final int GKB_WEIGHT = 1;
-   private static final int SPE_WEIGHT = 4;
-   private static final int STA_WEIGHT = 3;
+   // weight of attributes on overall, specific attribute listed
+   private static final int SHO_WEIGHT = 5;  // shooting weight
+   private static final int ANT_WEIGHT = 5;  // anticipation weight
+   private static final int BCT_WEIGHT = 3;  // ball control weight
+   private static final int PAS_WEIGHT = 3;  // passing weight
+   private static final int TAC_WEIGHT = 2;  // tackling weight
+   private static final int INT_WEIGHT = 1;  // interceptions weight
+   private static final int GKR_WEIGHT = 1;  // goalkeeping reactions weight
+   private static final int GKB_WEIGHT = 1;  // goalkeeping blocking weight
+   private static final int SPE_WEIGHT = 4;  // speed weight
+   private static final int STA_WEIGHT = 3;  // stamina weight
+
+   // get the sum of the weights
    private static final int SUM = SHO_WEIGHT + ANT_WEIGHT + BCT_WEIGHT + PAS_WEIGHT + TAC_WEIGHT + INT_WEIGHT + GKR_WEIGHT +
                                  GKB_WEIGHT + SPE_WEIGHT + STA_WEIGHT;
 
+   /*
+    * Attacker();
+    * Default constructor
+    * Delegates construction up one level
+    */
    public Attacker() {
-      Attacker("", "", "", new Date(), 0, new NationalTeam());
+      this("", "", "", new Date(), 0, new NationalTeam());
    }
 
+   /*
+    * Attacker(String,String,String,Date,int,Team);
+    * Constructor
+    * Delegates construction up one level
+    */
    public Attacker(String firstName, String lastName, String nickName, Date birthday,
                   int jerseyNum, Team nationalTeam) {
-      Attacker(firstName, lastName, nickName, birthday, jerseyNum, nationalTeam,
+      this(firstName, lastName, nickName, birthday, jerseyNum, nationalTeam,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
    }
 
+   /*
+    * Attacker(String,String,String,Date,int,Team,int,int,int,int,int,int,int,int,int,int);
+    * Main Constructor
+    * Calls main constructor of Player parent class with all variables
+    */
    public Attacker(String firstName, String lastName, String nickName, Date birthday,
                   int jerseyNum, Team nationalTeam,
                   int shooting, int anticipation, int ballControl, int passing,
@@ -32,14 +50,27 @@ public class Attacker extends Player {
          gkReactions, gkBlocking, speed, stamina);
    }
 
+   /*
+    * getRole();
+    * @return - ROLE enum of Player, notably Attacker
+    */
    public ROLE getRole() {
-      return ATT;
+      return Player.ROLE.ATT;
    }
 
-   private boolean calculateOverall() {
-      m_overall = SHO_WEIGHT * m_shooting + ANT_WEIGHT * m_anticipation + BCT_WEIGHT * m_ballControl + PAS_WEIGHT * m_passing +
-                  TAC_WEIGHT * m_tackling + INT_WEIGHT * m_interceptions + GKR_WEIGHT * m_gkReactions + GKB_WEIGHT * m_gkBlocking +
-                  SPE_WEIGHT * m_speed + STA_WEIGHT * m_stamina;
+   /*
+    * calculateOverall();
+    * @return - boolean, true if successfully sets overall, false otherwise
+    *
+    * Overrides abstract method from Player parent class.
+    */
+   protected boolean calculateOverall() {
+      if(!this.setOverall(SHO_WEIGHT * this.getShooting() + ANT_WEIGHT * this.getAnticipation() + BCT_WEIGHT * this.getBallControl() + PAS_WEIGHT * this.getPassing() +
+                  TAC_WEIGHT * this.getTackling() + INT_WEIGHT * this.getInterceptions() + GKR_WEIGHT * this.getGKReactions() + GKB_WEIGHT * this.getGKBlocking() +
+                  SPE_WEIGHT * this.getSpeed() + STA_WEIGHT * this.getStamina())) {
+         System.err.println("Error with setting overall");
+         return false;
+      }
 
       return true;
    }
