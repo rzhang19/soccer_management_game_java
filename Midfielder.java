3f9eb67be06ec0594a2831d83f226ec1a3a0dc3d
Robin Zhang <rzhang19@users.noreply.github.com>
2018-06-26 18:50:41 -0400

    Added comments for clarity

--- Midfielder.java
+++ Midfielder.java
@@ -1,27 +1,53 @@
+/*
+ * Midfielder.java
+ * Robin Zhang, 2018
+ * Soccer Management Game, Java
+ *
+ * Midfielder class, child of Player class
+ */
+
 public class Midfielder extends Player {
-   private static final int SHO_WEIGHT = 3;
-   private static final int ANT_WEIGHT = 2;
-   private static final int BCT_WEIGHT = 5;
-   private static final int PAS_WEIGHT = 5;
-   private static final int TAC_WEIGHT = 4;
-   private static final int INT_WEIGHT = 3;
-   private static final int GKR_WEIGHT = 1;
-   private static final int GKB_WEIGHT = 1;
-   private static final int SPE_WEIGHT = 3;
-   private static final int STA_WEIGHT = 5;
+   // weight of attributes on overall, specific attribute listed
+   private static final int SHO_WEIGHT = 3;  // shooting weight
+   private static final int ANT_WEIGHT = 2;  // anticipation weight
+   private static final int BCT_WEIGHT = 5;  // ball control weight
+   private static final int PAS_WEIGHT = 5;  // passing weight
+   private static final int TAC_WEIGHT = 4;  // tackling weight
+   private static final int INT_WEIGHT = 3;  // interceptions weight
+   private static final int GKR_WEIGHT = 1;  // goalkeeping reactions weight
+   private static final int GKB_WEIGHT = 1;  // goalkeeping blocking weight
+   private static final int SPE_WEIGHT = 3;  // speed weight
+   private static final int STA_WEIGHT = 5;  // stamina weight
+
+   // get the sum of the weights
    private static final int SUM = SHO_WEIGHT + ANT_WEIGHT + BCT_WEIGHT + PAS_WEIGHT + TAC_WEIGHT + INT_WEIGHT + GKR_WEIGHT +
                                  GKB_WEIGHT + SPE_WEIGHT + STA_WEIGHT;
 
+   /*
+    * Midfielder();
+    * Default constructor
+    * Delegates construction up one level
+    */
    public Midfielder() {
-      new Midfielder("", "", "", new Date(), 0, new NationalTeam());
+      this("", "", "", new Date(), 0, new NationalTeam());
    }
 
+   /*
+    * Midfielder(String,String,String,Date,int,Team);
+    * Constructor
+    * Delegates construction up one level
+    */
    public Midfielder(String firstName, String lastName, String nickName, Date birthday,
                   int jerseyNum, Team nationalTeam) {
-      Midfielder(firstName, lastName, nickName, birthday, jerseyNum, nationalTeam,
+      this(firstName, lastName, nickName, birthday, jerseyNum, nationalTeam,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
    }
 
+   /*
+    * Midfielder(String,String,String,Date,int,Team,int,int,int,int,int,int,int,int,int,int);
+    * Main Constructor
+    * Calls main constructor of Player parent class with all variables
+    */
    public Midfielder(String firstName, String lastName, String nickName, Date birthday,
                   int jerseyNum, Team nationalTeam,
                   int shooting, int anticipation, int ballControl, int passing,
@@ -32,14 +58,29 @@ public class Midfielder extends Player {
          gkReactions, gkBlocking, speed, stamina);
    }
 
+   /*
+    * getRole();
+    * Returns the ROLE of this Player, hardcoded as Midfielder
+
+    * @return - ROLE enum of Player
+    */
    public ROLE getRole() {
-      return MID;
+      return Player.ROLE.MID;
    }
 
-   private boolean calculateOverall() {
-      m_overall = SHO_WEIGHT * m_shooting + ANT_WEIGHT * m_anticipation + BCT_WEIGHT * m_ballControl + PAS_WEIGHT * m_passing +
-                  TAC_WEIGHT * m_tackling + INT_WEIGHT * m_interceptions + GKR_WEIGHT * m_gkReactions + GKB_WEIGHT * m_gkBlocking +
-                  SPE_WEIGHT * m_speed + STA_WEIGHT * m_stamina;
+   /*
+    * calculateOverall();
+    * Overrides abstract method from Player parent class.
+    *
+    * @return - boolean, true if successfully sets overall, false otherwise
+    */
+   protected boolean calculateOverall() {
+      if(!this.setOverall(SHO_WEIGHT * this.getShooting() + ANT_WEIGHT * this.getAnticipation() + BCT_WEIGHT * this.getBallControl() + PAS_WEIGHT * this.getPassing() +
+                  TAC_WEIGHT * this.getTackling() + INT_WEIGHT * this.getInterceptions() + GKR_WEIGHT * this.getGKReactions() + GKB_WEIGHT * this.getGKBlocking() +
+                  SPE_WEIGHT * this.getSpeed() + STA_WEIGHT * this.getStamina())) {
+         System.err.println("Error with setting overall");
+         return false;
+      }
 
       return true;
    }
--- NationalTeam.java
+++ NationalTeam.java
@@ -1,33 +1,83 @@
+/*
+ * NationalTeam.java
+ * Robin Zhang, 2018
+ * Soccer Management Game, Java
+ *
+ * NationalTeam class, child of Team class
+ */
+
 public class NationalTeam extends Team {
+   // maximum size of team
    public int MAX_NATIONAL_TEAM_SIZE = 23;
 
+   // adjective description of nationality
    private String m_nationality;
 
+   /*
+    * NationalTeam();
+    * Default constructor
+    * Delegates construction up one level
+    */
    public NationalTeam() {
-      NationalTeam("");
+      this("");
    }
 
+   /*
+    * NationalTeam(String);
+    * Constructor
+    * Delegates construction up one level
+    */
    public NationalTeam(String name) {
-      NationalTeam(name, "");
+      this(name, "");
    }
 
+   /*
+    * NationalTeam(String,String);
+    * Constructor
+    * Delegates construction up one level
+    */
    public NationalTeam(String name, String code) {
-      NationalTeam(name, code, "");
+      this(name, code, "");
    }
 
+   /*
+    * NationalTeam(String,String,String);
+    * Constructor
+    * Delegates construction up one level
+    */
    public NationalTeam(String name, String code, String continent) {
-      NationalTeam(name, code, continent, "");
+      this(name, code, continent, "");
    }
 
+   /*
+    * NationalTeam(String,String,String,String);
+    * Main constructor
+    * Calls main constructor of Team parent class
+    */
    public NationalTeam(String name, String code, String continent, String nationality) {
       super(name, code, continent);
+
+      m_nationality = nationality;
    }
 
+   /*
+    * setNationality(String);
+    * Sets the nationality to the parameter value
+    *
+    * @args (1) - String contains the nationality value
+    * @return - boolean, true if nationality stored successfully, false otherwise
+    */
    public boolean setNationality(String nationality) {
       m_nationality = nationality;
       return true;
    }
 
+   /*
+    * getNationality();
+    * Returns the nationality of this NationalTeam
+    *
+    * @return - String, returns m_nationality
+    */
    public String getNationality() {
       return m_nationality;
    }
