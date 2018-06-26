cebb9dffd96d088cc8acdd5398fe9c792269d42b
Robin Zhang <rzhang19@users.noreply.github.com>
2018-06-25 20:44:44 -0400

    Implemented match playing components
    Friendly matches are now 90 minutes long

--- FriendlyMatch.java
+++ FriendlyMatch.java
@@ -1,3 +1,21 @@
 public class FriendlyMatch extends Match {
-   
+   public FriendlyMatch() {
+      FriendlyMatch(new Team(), new Team());
+   }
+
+   public FriendlyMatch(Team team1, Team team2) {
+      super(team1, team2);
+
+      setType(FRIENDLY);
+   }
+
+   public boolean playMatch() {
+      playNinetyMinutes();
+   }
+
+   private boolean processMatch() {
+      getTeam1().processMatch(getScore1(), getScore2());
+      getTeam2().processMatch(getScore2(), getScore1());
+      return true;
+   }
 }
