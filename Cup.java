113013a34a2b0dda560bc7f7f4f703f5bfd855dc
Robin Zhang <rzhang19@users.noreply.github.com>
2018-07-02 15:33:15 -0400

    Created new file for cup tournaments

---
+++ Cup.java
@@ -0,0 +1,134 @@
+/*
+ * Cup.java
+ * Robin Zhang, 2018
+ * Soccer Management Game, Java
+ *
+ * Cup class holds data for any cup competition
+ */
+public class Cup {
+   // static ID counter
+   private static int idCount = 0;
+
+   // other static constants
+   private static final boolean DEFAULT_GROUPS = false;
+   private static final boolean DEFAULT_TWO_LEGS = false;
+   private static final int DEFAULT_NUM_GROUPS = 4;
+   private static final int DEFAULT_GROUP_SIZE = 4;
+   private static final int DEFAULT_GROUP_TIMES_PLAY = 1;
+   private static final int DEFAULT_QUALIFY_FROM_GROUP = 2;
+   private static final boolean DEFAULT_THIRD_PLACE = true;
+   private static final boolean DEFAULT_NUM_KNOCKOUT = 32;
+
+   // identification information
+   private String m_name;
+   private int m_id;
+
+   // group stage information
+   private Team[][] m_groups;
+   private boolean m_groups;
+   private int m_numGroups;
+   private int m_groupSize;
+   private int m_groupTimesPlay;
+   private int m_qualifyFromGroups;
+
+   // knockout information
+   private Team[] m_knockout;
+   private boolean m_thirdPlace;
+   private boolean m_twoLegs;
+   private int m_numKnockoutTeams;
+
+   // general information
+   private int m_totalMatches;
+
+   // cup running mechanics
+   private int m_started;
+
+   public Cup() {
+      this("");
+   }
+
+   public Cup(String name) {
+      this(name, DEFAULT_TWO_LEGS);
+   }
+
+   public Cup(String name, boolean twoLegs) {
+      this(name, twoLegs, DEFAULT_THIRD_PLACE);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace) {
+      this(name, twoLegs, thirdPlace, DEFAULT_GROUPS);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, boolean groups) {
+      this(name, twoLegs, thirdPlace, groups, DEFAULT_NUM_GROUPS);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, int numKnockoutTeams) {
+      m_name = name;
+      m_twoLegs = twoLegs;
+      m_thirdPlace = thirdPlace;
+
+      int numKnockout = numKnockoutTeams;
+      boolean goodNumber = true;
+      while (numKnockout != 0 && goodNumber) {
+         numKnockout /= 2;
+         if (numKnockout == 1) {
+            goodNumber = false;
+         }
+      }
+
+      if (goodNumber) {
+         m_numKnockoutTeams = numKnockoutTeams;
+      }
+
+      else {
+         m_numKnockoutTeams = DEFAULT_NUM_KNOCKOUT;
+      }
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, boolean groups,
+            int numGroups) {
+      this(name, twoLegs, thirdPlace, groups, numGroups, DEFAULT_GROUP_SIZE);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, boolean groups,
+            int numGroups, int groupSize) {
+      this(name, twoLegs, thirdPlace, groups, numGroups, groupSize,
+            DEFAULT_GROUP_TIMES_PLAY);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, boolean groups,
+            int numGroups, int groupSize, int groupTimesPlay) {
+      this(name, twoLegs, thirdPlace, groups, numGroups, groupSize,
+            groupTimesPlay, DEFAULT_QUALIFY_FROM_GROUP);
+   }
+
+   public Cup(String name, boolean twoLegs, boolean thirdPlace, boolean groups,
+            int numGroups, int groupSize, int groupTimesPlay, int qualifyFromGroups) {
+      m_name = name;
+      m_twoLegs = twoLegs;
+      m_thirdPlace = thirdPlace;
+      m_groups = groups;
+
+      m_numGroups = m_numGroups;
+      m_groupSize = groupSize;
+      m_groupTimesPlay = groupTimesPlay;
+      m_qualifyFromGroups = qualifyFromGroups;
+   }
+
+   private boolean calculateTotalMatches() {
+
+   }
+
+   public boolean setNumKnockoutTeams(int numKnockoutTeams) {
+
+   }
+
+   public boolean setName() {
+
+   }
+
+   public String getName() {
+      return m_name;
+   }
+}
